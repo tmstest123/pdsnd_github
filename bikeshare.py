ba8a857528306868ef5a3367b23f7c6fcb517329
Thomas Sojda <thomas@ts-apps.net>
2018-11-16 09:20:45 +0100

    docs: Improved get_filters parameter description

--- bikeshare.py
+++ bikeshare.py
@@ -12,7 +12,7 @@ def get_filters():
     Asks user to specify a city, month, and day to analyze.
 
     Returns:
-        (str) city - name of the city to analyze
+        (str) city - name of the city to analyze. Can be "chicago" "new york city" or "washington"
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
